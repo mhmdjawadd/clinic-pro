a5ee45a2f137d6463f488e83b490a03c844abbbb
Mohammad Jawad Wehbe <mhw07@mail.aub.edu>
2025-10-14 23:44:47 +0300

    added patient education card

---
+++ clinic-pro/src/patient_education_card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function PatientEducationCard() {
+  return (
+    <div className="flex flex-col justify-center items-center p-[14px_19px] gap-[10px] w-[750px] h-[245px] bg-white shadow-md rounded-[10px]">
+      {/* Inner Container */}
+      <div className="flex flex-col items-center w-[698px] h-[215px]">
+        {/* Header Section */}
+        <div className="flex flex-col items-start gap-[12px] w-[698px] h-[62px]">
+          {/* Title and Line */}
+          <div className="flex flex-col items-start gap-[3px] w-full h-[31px]">
+            <h2 className="text-[18px] font-normal text-black">Patient Education</h2>
+            <div className="border-b border-[#DEE2E6] w-full"></div>
+          </div>
+
+          {/* Coming Soon */}
+          <span className="text-[16px] text-black">Coming soon</span>
+        </div>
+
+        {/* Illustration */}
+        <div className="mt-[10px]">
+          <img
+            src="/path/to/range-of-motion-restoration.png"
+            alt="Range of Motion Restoration"
+            className="w-[318px] h-[153px] object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
